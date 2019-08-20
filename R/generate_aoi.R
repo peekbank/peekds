8ce32c1088a93f520b49d0e78df148192ff28683
Michael C. Frank <mcfrank@stanford.edu>
2019-08-20 11:57:44 -0700

    hot mess for resampling and interpretation

--- R/generate_aoi.R
+++ R/generate_aoi.R
@@ -26,17 +26,55 @@ generate_aoi <- function(dir) {
     mutate(t_trial = t - t[1],
            t_zeroed = t_trial - point_of_disambiguation)
 
+
+  ###### HOT MESS STARTS HERE #######
   # downsample to 30Hz
-  foo <- zoo(x = sin(1:10),
-                lubridate::parse_date_time((1:10) * 1/60,"s"))
-  foo_ts <- xts::xts(foo)
-  xts::endpoints(foo_ts, "ms", 30)
+  foo <- tsibble::as_tsibble(
+    tibble(aoi = c(rep("target", 5), NA, rep("distractor", 5), "other"),
+           t = c(lubridate::origin +
+                   lubridate::seconds(1/30)*(1:10),
+                 lubridate::origin +
+                   lubridate::seconds(1/30)*14,
+                 lubridate::origin +
+                   lubridate::seconds(1/30)*24)),
+    index = t, regular = FALSE) %>%
+    mutate(t_num = as.numeric(t))
+
+  sample_rate <- as.numeric(lubridate::round_date(lubridate::origin + lubridate::seconds(1/30),
+                                       ".000001 sec"))
+  sample_rate_crude <- as.numeric(lubridate::round_date(lubridate::origin + lubridate::seconds(1/30),
+                                                  ".001 sec"))
+
+  bar <- foo %>%
+    tsibble::index_by(
+      t2 = ~ lubridate::round_date(., paste0(sample_rate," sec"))) %>%
+    summarise(aoi2 = aoi[1]) %>%
+    tsibble::fill_gaps()  %>%
+    tsibble::index_by(
+      t3 = ~ lubridate::round_date(., paste0(sample_rate_crude," sec"))) %>%
+    summarise(aoi3 = na_mode(aoi2)) %>%
+    tsibble::slide_dfr(~ fill_gap(aoa3), size = 4) %>%
+    mutate(t_num = as.numeric(t3))
+
 
   # interpolate
 
-    # write aoi_data
+  # write aoi_data
 }
 
+fill_gap <- function (x) {
+  FIXME
+}
+
+na_mode <- function (x) {
+  if (all(is.na(x))) {
+    return(as.character(NA))
+  } else {
+    return(x[!is.na(x)][1])
+  }
+}
+
+###### HOT MESS ENDS HERE #######
 
 #' Generate aoi data from xy data
 #'
